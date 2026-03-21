79a70c5561fe934c5a97bb4492d829a2324fb7de
METAINJI <pcofjeongchan@gmail.com>
2026-03-22 01:36:50 +0900

    Update web.py

--- web.py
+++ web.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-app.mount("/static", StaticFiles(directory="static"), name=
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
